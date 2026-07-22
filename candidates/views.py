07f251292a35c9e7363939ce4736cfdbfaa02e57
Erfan Mohammadi <erfan.mohammadi.alv77@gmail.com>
2026-07-23 01:59:40 +0330

    feat: add candidate search functionality

--- candidates/views.py
+++ candidates/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .models import Candidate, WorkExperience, Education, Skill
@@ -13,6 +15,20 @@ class CandidateViewSet(viewsets.ModelViewSet):
     queryset = Candidate.objects.all()
     serializer_class = CandidateSerializer
     permission_classes = [IsAuthenticated]
+
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+    ]
+
+    search_fields = [
+        "first_name",
+        "last_name",
+        "email",
+        "summary",
+        "work_experiences__company_name",
+        "skills__name",
+    ]
 
 class WorkExperienceViewSet(viewsets.ModelViewSet):
     queryset = WorkExperience.objects.all()
